0ec366e112a50592506a1ca4a0f03b1a6db66c28
triconquest <juzzchg1092o0@gmail.com>
2025-05-27 14:18:24 +0400

    cleanup

--- Globals/Globals.h
+++ Globals/Globals.h
@@ -144,33 +144,10 @@ namespace I
 	inline IMatSystemSurface* Surface = CInterface::GetInterface<IMatSystemSurface>("VGUI_Surface030", "vguimatsurface.dll");
 	inline IGameEventManager2* GameEvent = CInterface::GetInterface<IGameEventManager2>("GAMEEVENTSMANAGER002", "engine.dll");
 	inline ICvar* Cvar = CInterface::GetInterface<ICvar>("VEngineCvar004", "vstdlib.dll");
-	//inline IMaterialSystem* MaterialSystem = CInterface::GetInterface<IMaterialSystem>("VMaterialSystem080", "MaterialSystem.dll");
 	inline IVModelRender* ModelRender = CInterface::GetInterface<IVModelRender>("VEngineModel016", "engine.dll");
 	inline IEngineTrace* EngineTrace = CInterface::GetInterface<IEngineTrace>("EngineTraceClient003", "engine.dll");
 	extern CInput* Input;
 	extern CGlobalVarsBase* Global;
-	//extern IClientMode* ClientMode;
-	//extern CClientModeShared* ClientModeShared;
 	inline int iScreenHeight = BaseClientDLL->GetScreenHeight();
 	inline int iScreenWidth = BaseClientDLL->GetScreenWidth();
 }
-
-/*I::BaseClientDLL = U::Interface.Get<IBaseClientDLL*>("client.dll", "VClient017");
-		I::Input = **reinterpret_cast<CInput***>((*reinterpret_cast<uintptr_t**>(I::BaseClientDLL))[15] + 0x2);
-		I::ClientEntityList = U::Interface.Get<IClientEntityList*>("client.dll", "VClientEntityList003");
-		I::GameMovement = U::Interface.Get<IGameMovement*>("client.dll", "GameMovement001");
-		I::ClientPrediction = U::Interface.Get<CPrediction*>("client.dll", "VClientPrediction001");
-		I::EngineClient = U::Interface.Get<IVEngineClient*>("engine.dll", "VEngineClient014");
-		I::ModelInfoClient = U::Interface.Get<IVModelInfoClient*>("engine.dll", "VModelInfoClient006");
-		I::EngineVGui = U::Interface.Get<IEngineVGui*>("engine.dll", "VEngineVGui001");
-		I::InputSystem = U::Interface.Get<IInputSystem*>("inputsystem.dll", "InputSystemVersion001");
-		I::EngineTrace = U::Interface.Get<IEngineTrace*>("engine.dll", "EngineTraceClient003");
-		I::RenderView = U::Interface.Get<IVRenderView*>("engine.dll", "VEngineRenderView014");
-		I::ModelRender = U::Interface.Get<IVModelRender*>("engine.dll", "VEngineModel016");
-		I::GameEventManager = U::Interface.Get<IGameEventManager2*>("engine.dll", "GAMEEVENTSMANAGER002");
-		I::StudioRender = U::Interface.Get<IStudioRender*>("studiorender.dll", "VStudioRender025");
-		I::MatSystemSurface = U::Interface.Get<IMatSystemSurface*>("vguimatsurface.dll", "VGUI_Surface030");
-		I::MaterialSystem = U::Interface.Get<IMaterialSystem*>("MaterialSystem.dll", "VMaterialSystem080");
-		I::Cvar = U::Interface.Get<ICvar*>("vstdlib.dll", "VEngineCvar004");
-		I::ClientMode = **(ClientModeShared***)(U::Pattern.Find("client.dll", "8B 0D ? ? ? ? 8B 01 5D FF 60 28 CC") + 0x2);
-		I::HudChat = (CHudChat*)(((uintptr_t*)(I::ClientMode))[4]);*/
